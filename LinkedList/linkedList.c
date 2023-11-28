1d12277c6061d045e41b97250b8f0438738dffac
FlorianGrollich <F-Grollich@web.de>
2023-11-28 22:54:51 +0100

    feat: add add function to linkes list

--- LinkedList/linkedList.c
+++ LinkedList/linkedList.c
@@ -3,13 +3,21 @@
 #include <stdlib.h>
 
 struct Node* createNode(int value) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->value = value;
+    newNode->next = NULL;
 }
 
 
-void add(struct Node head, int value) {
-    if (head.next == NULL) {
+void add(struct Node* head, int value) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->next = NULL;
+    newNode->value = value;
 
+    while (head->next != NULL) {
+        head = head->next;
     }
+    head->next = newNode;
 }
 
 
